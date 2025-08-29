44763e94d1fc7ce60e96d1511933cb07c868ac6b
fek247 <ngovantaino2000@gmail.com>
2025-08-29 07:37:10 +0000

    websocket server

--- BE/src/main.rs
+++ BE/src/main.rs
@@ -1,73 +1,125 @@
-use axum::{routing::post, Json, Router};
-use jsonwebtoken::{encode, EncodingKey, Header};
-use serde::{Deserialize, Serialize};
+use std::{collections::HashSet, sync::{Arc, Mutex}};
+
+use axum::{extract::{ws::{Message, Utf8Bytes, WebSocket}, State, WebSocketUpgrade}, response::{Html, IntoResponse}, routing::get, Router};
+use futures_util::{SinkExt, StreamExt};
+use tokio::sync::broadcast;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+
+// Our shared state
+struct AppState {
+    // We require unique usernames. This tracks which usernames have been taken.
+    user_set: Mutex<HashSet<String>>,
+    // Channel used to send messages to all connected clients.
+    tx: broadcast::Sender<String>,
+}
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=trace", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // Set up application state for use with with_state().
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(100);
+
+    let app_state = Arc::new(AppState{user_set, tx});
+
     let app = Router::new()
-        .route("/authorize", post(authorize));
+        .route("/", get(index))
+        .route("/websocket", get(websocket_handler))
+        .with_state(app_state);
+    
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+            .await
+            .unwrap();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
-
-    println!("listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn authorize(Json(payload): Json<AuthPayload>) -> Result<Json<AuthBody>, AuthError> {
-    if payload.client_id.is_empty() || payload.client_secret.is_empty() {
-        return Err(AuthError::MissingCredentials);
-    }
-
-    if payload.client_id != "foo" || payload.client_secret != "bar" {
-        return Err(AuthError::WrongCredentials);
-    }
-
-    let claims = Claims {
-        sub: "b@b.com".to_owned(),
-        company: "ACME".to_owned(),
-        exp: 2000000000, // May 2033
-    };
-
-    let token = encode(&Header::default(), &claims, &EncodingKey::from_secret("secret".as_ref()))
-        .map_err(|_| AuthError::TokenCreation)?;
-
-    Ok(Json(AuthBody::new(token)))
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<AppState>>
+) -> impl IntoResponse {
+    ws.on_upgrade(|socket| websocket(socket, state))
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    company: String,
-    exp: usize,
-}
+async fn websocket(stream: WebSocket, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = stream.split();
 
-#[derive(Debug, Serialize)]
-struct AuthBody {
-    access_token: String,
-    token_type: String
-}
+    let mut username = String::new();
 
-#[derive(Debug, Deserialize)]
-struct AuthPayload {
-    client_id: String,
-    client_secret: String,
-}
+    while let Some(Ok(message)) = receiver.next().await {
+        if let Message::Text(name) = message {
+            check_username(&state, &mut username, name.as_str());
 
-#[derive(Debug)]
-enum AuthError {
-    WrongCredentials,
-    MissingCredentials,
-    TokenCreation,
-    InvalidToken,
-}
+            if !username.is_empty() {
+                break;
+            } else {
+                let _ = sender
+                    .send(Message::Text(Utf8Bytes::from_static(
+                        "Username already taken.",
+                    )))
+                    .await;
 
-impl AuthBody {
-    fn new(access_token: String) -> Self{
-        Self {
-            access_token,
-            token_type: "Bearer".to_string()
+                return;
+            }
         }
     }
+
+    let mut rx = state.tx.subscribe();
+
+    let msg = format!("{username} joined");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if sender.send(Message::text(msg)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    let tx = state.tx.clone();
+    let name = username.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            let _ = tx.send(format!("{name}: {text}"));
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort(),
+    };
+
+    let msg = format!("{username} left");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    state.user_set.lock().unwrap().remove(&username);
 }
+
+fn check_username(state: &AppState, string: &mut String, name: &str) {
+    let mut user_set = state.user_set.lock().unwrap();
+
+    if !user_set.contains(name) {
+        user_set.insert(name.to_owned());
+
+        string.push_str(name);
+    }
+}
+
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../chat.html"))
+}
+
+
